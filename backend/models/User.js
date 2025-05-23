5f5feb1d42f981a42f362a67a7543df23fb034db
kanisiel2 <kanisiel@gmail.com>
2025-05-23 09:31:26 +0900

    Add missing files

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,9 +2,9 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../models');
 
-const User = sequelize.define('User', {
-  username: { type: DataTypes.STRING, allowNull: false, unique: true },
-  password: { type: DataTypes.STRING, allowNull: false }
-});
+    const User = (DataTypes, sequelize) => { sequelize.define('User', {
+    username: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false }
+    }); }
 
 module.exports = User;
