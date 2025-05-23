d3275e5b5f381b4bc92b1010a19bbfa930f49784
kanisiel2 <kanisiel@gmail.com>
2025-05-23 11:03:19 +0900

    change routes

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -5,8 +5,9 @@ const clientController = require('../controllers/clientController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.use(authMiddleware);
-router.get('/', clientController.getClients);
+router.get('/', clientController.getAllClients);
 router.post('/', clientController.registerClient);
+router.get('/:id', clientController.getClient);
 router.put('/:id', clientController.updateClient);
 router.delete('/:id', clientController.deleteClient);
 
