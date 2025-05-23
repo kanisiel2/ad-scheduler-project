90f9fbea8ee57559a8dbd7ab474ac0f5e3b4be26
kanisiel2 <kanisiel@gmail.com>
2025-05-23 11:15:43 +0900

    add console log

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,6 +2,8 @@
 const { Sequelize } = require('sequelize');
 const config = require('../config');
 
+console.log(config)
+
 const sequelize = new Sequelize(config.DB_NAME, config.DB_USER, config.DB_PASS, {
   host: config.DB_HOST,
   dialect: 'mariadb'
